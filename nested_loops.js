db8a37ce21823cb20ae1c373bfa56913922af0ff
Alex <38843286+BeAllAround@users.noreply.github.com>
2022-11-03 01:03:04 +0100

    Add files via upload

---
+++ nested_loops.js
@@ -0,0 +1,40 @@
+function linear_nested(size) {
+	let arr = []
+
+	let i
+	for(i = 0; i < size; i++) {
+		arr.push(i)
+	}
+
+
+	let start = Date.now()
+	for(let item of arr) {
+		arr.find(v=>v==item)
+	}
+	console.log("linear passed: ", (Date.now() - start) / 1000)
+	// O(n^2)
+}
+
+function hash_nested() {	
+	let arr = []
+
+	let i
+	for(i = 0; i < size; i++) {
+		arr.push(i)
+	}
+
+	let arr_hash = arr.reduce((obj, v) => Object.assign(obj, 
+		{[v]: true,}), {})
+	// console.log(Object.keys(arr_hash).length)
+
+	let start = Date.now()
+	for(let item of arr) {
+		arr_hash[item]
+	}
+	console.log("hash passed: ", (Date.now() - start) / 1000)
+	// O(n+n)
+}
+
+let size = 10000
+linear_nested(size)
+hash_nested(size)
